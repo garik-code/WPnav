3fb3f1b27f2d57af9fdc5a9f20d9bba79f4b02d1
Игорь Мещеряков <maiL@garik.site>
2019-04-24 18:32:28 +0400

    %)

--- lib/nav.php
+++ lib/nav.php
@@ -3,6 +3,9 @@
   <div class="content" id="content">
     <?php for($i=0; $i < count($cat); $i++): ?>
       <?php if($index_cat[$cat[$i]['term_id']]): ?>
+
+
+        
     <div class="block">
       <?php $cat_mama = $cat[$i]['slug']; ?>
       <a href="/category/<?php print $cat[$i]['slug']; ?>/" onClick="catalog('catalog_<?php print $cat[$i]['term_id']; ?>', '/category/<?php print $cat[$i]['slug']; ?>/');return false;">
@@ -11,18 +14,25 @@
       <div id="catalog_<?php print $cat[$i]['term_id']; ?>" style="display: none;">
         <div class="block" style="background-color: #d6d6d6;">
           <a href="/category/<?php print $cat_mama; ?>/">
-            <h2>Все товары</h2>
+            <h2>Все объявления</h2>
           </a>
         </div>
+
+
+
         <?php for ($a=0; $a < count($tax); $a++): ?>
           <?php if($cat[$i]['term_id'] == $tax[$a]['parent']): ?>
             <?php for($b=0; $b < count($cat); $b++): ?>
               <?php if($tax[$a]['term_id'] == $cat[$b]['term_id']): ?>
-        <div class="block">
-          <a href="/category/<?php print $cat[$b]['slug']; ?>/">
-            <h2><?php print $cat[$b]['name']; ?></h2>
-          </a>
-        </div>
+
+
+                  <div class="block">
+                    <a href="/category/<?php print $cat[$b]['slug']; ?>/">
+                      <h2><?php print $cat[$b]['name']; ?></h2>
+                    </a>
+                  </div>
+
+
               <?php endif; ?>
             <?php endfor; ?>
           <?php endif; ?>
