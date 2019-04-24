691594d642eed9ce765b2ce2999dc90ca6f99d23
Игорь Мещеряков <maiL@garik.site>
2019-04-24 18:10:46 +0400

    (%

--- lib/nav.php
+++ lib/nav.php
@@ -1,24 +1,13 @@
 <nav id="navSmarty">
-  <h3>Каталог</h3>
   <div id="oldContent" style="display: none;"></div>
   <div class="content" id="content">
     <?php for($i=0; $i < count($cat); $i++): ?>
       <?php if($index_cat[$cat[$i]['term_id']]): ?>
     <div class="block">
       <?php $cat_mama = $cat[$i]['slug']; ?>
-      <?php if(mb_strtoupper($cat[$i]['name']) == 'КАМИНЫ'): ?>
-        <a href="http://kamin.volga-bereg.ru/" target="_blank">
-          <h2>КАМИНЫ</h2> <i class="fa fa-external-link-square" aria-hidden="true"></i>
-        </a>
-      <?php elseif(mb_strtoupper($cat[$i]['name']) == 'МАСТЕРСКАЯ КОВКИ'): ?>
-        <a href="http://masterkovki64.ru/" target="_blank">
-          <h2>МАСТЕРСКАЯ КОВКИ</h2> <i class="fa fa-external-link-square" aria-hidden="true"></i>
-        </a>
-      <?php else: ?>
       <a href="/category/<?php print $cat[$i]['slug']; ?>/" onClick="catalog('catalog_<?php print $cat[$i]['term_id']; ?>', '/category/<?php print $cat[$i]['slug']; ?>/');return false;">
         <h2><?php print $cat[$i]['name']; ?></h2> <i class="fa fa-chevron-right" aria-hidden="true"></i>
       </a>
-      <?php endif; ?>
       <div id="catalog_<?php print $cat[$i]['term_id']; ?>" style="display: none;">
         <div class="block" style="background-color: #d6d6d6;">
           <a href="/category/<?php print $cat_mama; ?>/">
